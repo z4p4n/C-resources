cfca86d15c11d33767cca6d640a2790ef8ec2a13
zapan <zapan@zapan.info>
2016-06-27 19:16:08 +0200

    cnfToHlpsl improvement

--- examples/cnftohlpsl/cnfToHlpsl.c
+++ examples/cnftohlpsl/cnfToHlpsl.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <errno.h>
+#include <math.h>
 
 #include "../../ZpnError.h"
 #include "../../ZpnString.h"
@@ -191,9 +192,124 @@ int parse_cnf_data (p_cnf cnf, char *data) {
 
 void disp_nice_message () {
 
-	fprintf (stdout, "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+	fprintf (stdout, "\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
 	                 "| Transform CNF to HLPSL by Zapan |\n"
 	                 "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n\n");
+}
+
+/* Transform ksat to hlpsl file                *
+ * ------------------------------------------- *
+ * p_cnf cnf  : struct who get cnf data        *
+ * char *path : output file                    *
+ * ------------------------------------------- *
+ * return ZPN_ERROR if fail, ZPN_OK otherwhise */
+int write_hlpsl_from_ksat (p_cnf cnf, const char *path) {
+
+	int i,j,k;
+	FILE *fd;
+
+	fprintf (stdout, "[?] Trying to transform sat formula to HLPSL\n");
+
+	fd = fopen (path, "w");
+	if (fd == NULL) {
+		perror ("fopen");
+		return ZPN_ERROR;
+	}
+
+	/* Write C0 role */
+	fprintf (fd, 
+	  "role role_C0(C0:agent,C1:agent,True,False,Key,MEnd:public_key,SND,RCV:channel(dy))\n"
+	  "played_by C0\ndef=\n\tlocal\n\t\tState:nat,Secret:text");
+	for (i = 1 ; i < cnf->lit + 1 ; i++) fprintf (fd, ",X%d:public_key", i);
+	fprintf (fd, "\n\tinit\n\t\tState := 0\n\ttransition\n\t\t1. State=0 /\\ RCV(");
+	for (i = 0 ; i < cnf->lit ; i++) {
+		if (i == 0) fprintf (fd, "X1'"); else fprintf (fd, ".X%d'", i + 1);
+	}
+	fprintf (fd, ") =|> State':=1 /\\ SND({");
+	for (i = 0 ; i < cnf->cla ; i++) {
+		for (j = 0 ; j < cnf->lpc ; j++) {
+			if (i != 0 || j != 0) fprintf (fd, ".");
+			fprintf (fd, "X%d'", abs (cnf->cnf[i][j])); 
+		}
+	}
+	fprintf (fd, ".MEnd}_Key)\n\t\t2. State=1 /\\ RCV(Secret') =|> State':=2 /\\ "
+	  "secret(Secret',sec,{})\nend role\n\n");
+
+	/* Write C1 to C(nb clauses) */
+	for (i = 0 ; i < cnf->cla ; i++) {
+		fprintf (fd, 
+		  "role role_C%d(C%d:agent,C%d:agent,True,False,Key,MEnd:public_key,SND,RCV:"
+		  "channel(dy))\nplayed_by C%d\ndef=\n\tlocal\n\t\tState:nat,Secret:text",i+1,i+1,i+2,i+1);
+		for (j = (i+1) * cnf->lpc ; j < cnf->cla * cnf->lpc ; j++) 
+			fprintf (fd, ",X%d:public_key", j);
+		for (j = 0 ; j < cnf->lpc ; j++) fprintf (fd, ",K%d:public_key", j);
+		fprintf (fd, "\n\tinit\n\t\tState := 0\n\ttransition\n");
+		for (j = 0 ; j < cnf->lpc ; j++) {
+			fprintf (fd, "\t\t%d. State=0 /\\ RCV({", j);
+			if (j == 0 && cnf->cnf[i][j] > 0) fprintf (fd, "True.K1'.K2'");
+			if (j == 0 && cnf->cnf[i][j] < 0) fprintf (fd, "False.K1'.K2'");
+			if (j == 1 && cnf->cnf[i][j] > 0) fprintf (fd, "K0'.True.K2'");
+			if (j == 1 && cnf->cnf[i][j] < 0) fprintf (fd, "K0'.False.K2'");
+			if (j == 2 && cnf->cnf[i][j] > 0) fprintf (fd, "K0'.K1'.True");
+			if (j == 2 && cnf->cnf[i][j] < 0) fprintf (fd, "K0'.K2'.False");
+
+			for (k = (i+1) * cnf->lpc ; k < cnf->cla * cnf->lpc ; k++) {
+				fprintf (fd, ".X%d'", k);
+			}
+			fprintf (fd, ".MEnd}_Key) =|> State':=1 /\\ SND({");
+			for (k = (i+1) * cnf->lpc ; k < cnf->cla * cnf->lpc ; k++) {
+				if (k == (i+1) * cnf->lpc) fprintf (fd, "X%d'", k);
+				else fprintf (fd, ".X%d'", k);
+			}
+			if (i == cnf->cla - 1) fprintf (fd, "MEnd}_Key)\n");
+			else                   fprintf (fd, ".MEnd}_Key)\n");
+		}
+		fprintf (fd, "end role\n\n");
+	}
+
+	/* Write End Role */
+	fprintf (fd, 
+	  "role role_End(C0:agent,End:agent,True,False,Key,MEnd:public_key,SND,RCV:channel(dy))\n"
+	  "played_by End\ndef=\n\tlocal\n\t\tState:nat,Secret:text\n\tinit\n\t\tState := 0\n"
+	  "\ttransition\n\t\t1. State=0 /\\ RCV({MEnd}_Key) =|> State':=1 /\\ Secret':=new() /\\ "
+	  "secret(Secret',sec,{}) /\\ SND(Secret')\nend role\n\n");
+	
+	/* Write session function role */
+	fprintf (fd, "role session(True:public_key,False:public_key,Key:public_key,MEnd:public_key");
+	for (i = 0 ; i < cnf->cla + 1 ; i++) fprintf (fd, ",C%d:agent", i);
+	fprintf (fd, ",End:agent)\ndef=\n\tlocal\n\t\t");
+	for (i = 0 ; i < cnf->cla + 1; i++) {
+		if (i == 0) fprintf (fd, "SND0,RCV0");
+		else fprintf (fd, ",SND%d,RCV%d", i, i);
+	}
+	fprintf (fd, ",SNDEnd,RCVEnd:channel(dy)\n\tcomposition\n\t\t");
+	for (i = 0 ; i < cnf->cla + 1 ; i++) {
+		if (i != 0) fprintf (fd, " /\\ ");
+		if (i == cnf->cla) 
+			fprintf (fd, "role_C%d(C%d,End,True,False,Key,MEnd,SND%d,RCV%d)", i, i, i, i);
+		else fprintf (fd, "role_C%d(C%d,C%d,True,False,Key,MEnd,SND%d,RCV%d)", i, i, i + 1, i, i);
+	}
+	fprintf (fd, " /\\ role_End(C0,End,True,False,Key,MEnd,SNDEnd,RCVEnd)\nend role\n\n");
+	
+	/* Write environment function role */
+	fprintf (fd, "role environment()\ndef=\n\tconst\n"
+		"\t\thash_0:function,pk1:public_key,pk2:public_key,pk3:public_key,pk4:public_key"
+	    ",sec:protocol_id");
+	for (i = 0 ; i < cnf->cla + 1; i++) fprintf (fd, ",c%d:agent", i);
+	fprintf (fd, ",theend:agent");
+	fprintf (fd, "\n\tintruder_knowledge = {pk1,pk2}\n\tcomposition\n\t\tsession(pk1,pk2,pk3,pk4");
+	for (i = 0 ; i < cnf->cla + 1 ; i++) fprintf (fd, ",c%d", i);
+	fprintf (fd, ",theend)\nend role\n\n");
+
+	/* Write goal */
+	fprintf (fd, "goal\n\tsecrecy_of sec\nend goal\n\n");
+	
+	/* Write calling environment instance */
+	fprintf (fd, "environment()\n");
+
+	fclose (fd);
+
+	return ZPN_OK;
 }
 
 int main (int argc, const char *argv[]) {
@@ -202,9 +318,9 @@ int main (int argc, const char *argv[]) {
 	char *data;
 	t_cnf cnf;
 
-	if (argc < 2) {
+	if (argc < 3) {
 
-		fprintf (stderr, "usage %s [CNF file]\n", argv[0]);
+		fprintf (stderr, "usage %s CNF_file output_file\n", argv[0]);
 		exit (EXIT_FAILURE);
 	}
 
@@ -219,15 +335,16 @@ int main (int argc, const char *argv[]) {
 
 	/* Parse CNF data */
 	ret = parse_cnf_data (&cnf, data);
-	if (ret == ZPN_ERROR) goto fail_parsing;
+	munmap (data, size);
+	if (ret == ZPN_ERROR) exit (EXIT_FAILURE);
 
+	/* Write HLPSL From k-Sat */
+	ret = write_hlpsl_from_ksat (&cnf, argv[2]);
 	free_cnf (&cnf);
-	munmap (data, size);
+	if (ret == ZPN_ERROR) exit (EXIT_FAILURE);
+
+	fprintf (stdout, "[+] Success\n");
+
 	exit (EXIT_SUCCESS);
-
-	fail_parsing :
-
-	munmap (data, size);
-	exit (EXIT_FAILURE);
 }
 
